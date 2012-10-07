0c143cadcad40f23b2f9e498842aaa0733c66f91
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-07 07:11:08 -0430

    added junit empty test

---
+++ shop/src/test/java/org/shop/api/OrderServiceTests.java
@@ -0,0 +1,38 @@
+package org.shop.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderServiceTests {
+
+    @Test
+    public void testCreateOrderUserItemArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateOrderUserProposalArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetOrderById() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdateOrder() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetOrdersByUser() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetOrdersByUserId() {
+        fail("Not yet implemented");
+    }
+}
