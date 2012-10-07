842a1fcecee6346f8701d37a3b64120355bf18ba
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-07 07:16:53 -0430

    added hints

--- shop/src/test/java/org/shop/api/OrderServiceTests.java
+++ shop/src/test/java/org/shop/api/OrderServiceTests.java
@@ -6,33 +6,41 @@ import org.junit.Test;
 
 public class OrderServiceTests {
 
+    OrderService orderService;
+    
     @Test
     public void testCreateOrderUserItemArray() {
         fail("Not yet implemented");
+        //orderService.createOrder(user, items);
     }
 
     @Test
     public void testCreateOrderUserProposalArray() {
         fail("Not yet implemented");
+        //orderService.createOrder(user, proposals);
     }
 
     @Test
     public void testGetOrderById() {
         fail("Not yet implemented");
+        //orderService.getOrderById(id);
     }
 
     @Test
     public void testUpdateOrder() {
         fail("Not yet implemented");
+        //orderService.updateOrder(order);
     }
 
     @Test
     public void testGetOrdersByUser() {
         fail("Not yet implemented");
+        //orderService.getOrdersByUser(user);
     }
 
     @Test
     public void testGetOrdersByUserId() {
         fail("Not yet implemented");
+        //orderService.getOrdersByUserId(userId);
     }
 }
