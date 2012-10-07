ffd4af062dced4f56653203d42d3d1ff49856317
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-07 07:28:10 -0430

    Create empty junit test case

---
+++ shop/src/test/java/org/shop/api/UserServiceTests.java
@@ -0,0 +1,34 @@
+package org.shop.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceTests {
+
+    UserService userService;
+    
+    @Test
+    public void testRegisterUser() {
+        fail("Not yet implemented");
+        //userService.registerUser(user);
+    }
+
+    @Test
+    public void testGetUserById() {
+        fail("Not yet implemented");
+        //userService.getUserById(id);
+    }
+
+    @Test
+    public void testUpdateUserProfile() {
+        fail("Not yet implemented");
+        //userService.updateUserProfile(user);
+    }
+
+    @Test
+    public void testGetUsers() {
+        fail("Not yet implemented");
+        //userService.getUsers();
+    }
+}
